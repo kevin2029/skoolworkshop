4b3d05bfb01860d2e73915e7b1f78df41598db00
Jay_Moll <jaymoll95@gmail.com>
2020-05-26 14:25:38 +0200

    sql createworkshop
    create workshop methoden veranderd zodat hij sql compatible is,  (zonder query)

--- src/controllers/workshop.controller.js
+++ src/controllers/workshop.controller.js
@@ -3,24 +3,59 @@ const config = require('../config/config')
 const assert = require('assert')
 
 let controller = {
+
     createWorkshop(req, res, next) {
-        console.log(method, 'post aangeroepen')
+        logger.info('createworkshop called')
         const workshop = req.body
+        let { name, description, tijdsduur, price } = workshop
+        console.log('workshop =', workshop)
 
-        try {
+        // !!
+        const userId = 1
 
+        let sqlQuery =
+        //query hier
+        logger.debug('createWorkshop', 'sqlQuery =', sqlQuery)
 
-
-            res.status(200).json({
-                result: workshop
-            })
-        } catch (error) {
+        pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log('createWorkshop', error)
             res.status(400).json({
-                message: 'error creating workshop',
-                error: error.toString()
+            message: 'createWorkshop failed getting connection!',
+            error: err
             })
         }
+        if (connection) {
+            // Use the connection
+            connection.query(
+            sqlQuery,
+            [name, description, tijdsduur, price, userId],
+            (error, results, fields) => {
+                // When done with the connection, release it.
+                connection.release()
+                // Handle error after the release.
+                if (error) {
+                console.log('createWorkshop', error)
+                res.status(400).json({
+                    message: 'createWorkshop failed calling query',
+                    error: error
+                })
+                }
+                if (results) {
+                console.log('results: ', results)
+                res.status(200).json({
+                    result: {
+                    id: results.insertId,
+                    ...movie
+                    }
+                })
+                }
+            }
+            )
+        }
+        })
     }
-
-    
+  
 }
+
+module.exports = controller
