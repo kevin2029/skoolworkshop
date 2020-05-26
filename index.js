e53a1e3ef6a26f9fa8b42b09c6a81e913fb521b9
Sam van der Flaas <s.vanderflaas@student.avans.nl>
2020-05-26 12:13:53 +0200

    hotfix

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const connection = require('./src/config/database');
+
+const workshoproutes = require('./src/routes/workshop.routes');
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.all('*', (req, res, next) => {
 });
 
 // routes
+app.use('/api', workshoproutes);
 
 app.all('*', (req, res, next) => {
   res.status(404).json({
@@ -22,8 +26,20 @@ app.all('*', (req, res, next) => {
   });
 });
 
-app.all('*', (req, res, next) => {
-  res.status(404).json({
-    error: 'Endpoint does not exist!'
+app.listen(port, () =>
+  logger.info(`Server listening at http://localhost:${port}`)
+);
+
+function gracefulShutdown() {
+  logger.info('Server shutting down');
+  connection.end(function (err) {
+    logger.info('Database pool connections closed');
   });
-});
+}
+
+// e.g. kill
+process.on('SIGTERM', gracefulShutdown);
+// e.g. Ctrl + C
+process.on('SIGINT', gracefulShutdown);
+
+module.exports = app;
