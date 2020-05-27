e3c9c2330ba2f1d78028e1fc8ae1030c40c622db
Jay_Moll <jaymoll95@gmail.com>
2020-05-27 11:31:51 +0200

    small changes

--- src/routes/workshop.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const workshopcontroller = require('../controllers/workshop.controller')
-
-router.post('/createworkshop', workshopcontroller.createWorkshop)
-
-module.exports = router
