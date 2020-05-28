3342b5fba646f1782f014d27059fb459838ff1ac
Jay_Moll <jaymoll95@gmail.com>
2020-05-28 13:58:31 +0200

    delete + create workshop
    edits op create en delete gemaakt

--- src/controllers/workshop.controller.js
+++ src/controllers/workshop.controller.js
@@ -22,39 +22,70 @@ let controller = {
     createWorkshop(req, res, next) {
         logger.info('createworkshop called')
         const workshop = req.body
-        let { name, description, tijdsduur, price } = workshop
+        let { naam, beschrijving, kosten, vervolgKosten, genre } = workshop
         console.log('workshop =', workshop)        
 
         let sqlQuery =
-        //query hier
+        'INSERT INTO `Workshop` (`Naam`, `Beschrijving`, `Kosten`, `Vervolg Kosten`, `genre`) VALUES (?, ?, ?, ?, ?)'
         logger.debug('createWorkshop', 'sqlQuery =', sqlQuery)
 
         connection.query(
             sqlQuery,
-            [name, description, tijdsduur, price],
+            [ naam, beschrijving, kosten, vervolgKosten, genre ],
             (error, results, fields) => {
-                // When done with the connection, release it.
+                
                 connection.release()
-                // Handle error after the release.
+                
                 if (error) {
                 console.log('createWorkshop', error)
                 res.status(400).json({
                     message: 'createWorkshop failed calling query',
                     error: error
-                })
+                    })
                 }
                 if (results) {
-                console.log('results: ', results)
-                res.status(200).json({
-                    result: {
-                    id: results.insertId,
-                    ...movie
-                    }
-                })
+                    console.log('results: ', results)
+                    res.status(200).json({
+                        result: {
+                            ...workshop
+                        }
+                    })
                 }
             }
-            )
-    }        
+        )
+    },
+    
+    deleteWorkshop(req, res, next) {
+        logger.info('deleteWorkshop called')
+        const workshopName = req.params.name
+
+        let sqlQuery = 
+        'DELETE FROM `Workshop` WHERE Naam =' + workshopName
+        logger.debug('deleteWorkshop', 'sqlQuery =', sqlQuery)
+
+        connection.query(
+            sqlQuery,
+            (error, results, fields) => {
+                connection.release()
+
+                if (error) {
+                    console.log('deleteWorkshop', error)
+                    res.status(400).json({
+                        message: 'deleteWorkshop failed',
+                        error: error
+                    })
+                }
+                if (results) {
+                    console.log('results: ', results)
+                    cres.status(200).json({
+                        result: {
+                            ...workshop
+                        }
+                    })
+                }
+            }
+        )
+    }
   
 }
 
--- src/routes/workshop.routes.js
+++ src/routes/workshop.routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const workshopcontroller = require('../controllers/workshop.controller')
 
 router.post('/create', workshopcontroller.validateWorkshop,workshopcontroller.createWorkshop)
+router.post('/delete', workshopcontroller.deleteWorkshop)
 
 module.exports = router
