7786c5819c5a5109e6e17e24c62da9b82c868d41
Sam van der Flaas <s.vanderflaas@student.avans.nl>
2020-05-28 15:30:57 +0200

    minor changes

--- gebruikersaanmaken/gebruikeraanmaken.js
+++ gebruikersaanmaken/gebruikeraanmaken.js
@@ -3,8 +3,26 @@
 
 function verstuur() {
   console.log("start");
-  let naam = document.getElementById("naam-beheerder");
-  console.log(naam);
+
+  const naam = document.getElementById("naam-beheerder").value;
+  const organisatie = document.getElementById("naam-organisatie").value;
+  const email = document.getElementById("email").value;
+  const wachtwoord = document.getElementById("wachtwoord").value;
+
+  const gegevens = {
+    Name: naam,
+    Email: email,
+    Organisation: organisatie,
+    Password: wachtwoord,
+  };
+
+  console.log(gegevens);
+
+  // POST request maken op server
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "localhost:3000/api/user", true);
+  xhttp.send(gegevens);
+
   console.log("stop");
   alert("The form was submitted");
 }
