8c4b12a073e74cbb4864623375886ac1bb90e8e7
Jay_Moll <jaymoll95@gmail.com>
2020-05-28 16:10:45 +0200

    aanpassing workshop frontend

--- workshopaanmaken/workshopaanmaken.js
+++ workshopaanmaken/workshopaanmaken.js
@@ -3,8 +3,28 @@
 
 function verstuur() {
   console.log("start");
-  let naam = document.getElementById("naam-workshop");
-  console.log(naam);
+
+  const naam = document.getElementById("naam-workshop").value;
+  const beschrijving = document.getElementById("beschrijving").value;
+  const prijs = document.getElementById("prijs").value;
+  const vervolgPrijs = document.getElementById("vervolg-prijs").value;
+  const genre = document.getElementById("genre").value;
+
+  const gegevens = {
+    Name: naam,
+    Beschrijving: beschrijving,
+    Prijs: prijs,
+    VervolgPrijs: vervolgPrijs,
+    Genre: genre
+  };
+
+  console.log(gegevens);
+
+  // POST request maken op server
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "localhost:3000/api/user", true);
+  xhttp.send(gegevens);
+
   console.log("stop");
   alert("The form was submitted");
 }
