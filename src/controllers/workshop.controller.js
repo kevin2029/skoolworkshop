c85d5b5c133ca12928e6089c100e017d34618eb5
Jay_Moll <jaymoll95@gmail.com>
2020-06-02 16:17:16 +0200

    workshop querries
    alle routes en controllers gemaakt

--- src/controllers/workshop.controller.js
+++ src/controllers/workshop.controller.js
@@ -6,10 +6,11 @@ let controller = {
     validateWorkshop(req, res, next) {
         try {
             const { name, description, tijdsduur, price } = req.body
-            assert(typeof name === 'string', 'name is missing.')
-            assert(typeof description === 'string', 'description is missing.')
-            assert(typeof tijdsduur === 'number', 'tijdsduur is missing.')
-            assert(typeof price === 'number', 'price is missing.')
+            assert(typeof naam === 'string', 'name is missing.')
+            assert(typeof beschrijving === 'string', 'description is missing.')
+            assert(typeof Kosten === 'number', 'tijdsduur is missing.')
+            assert(typeof vervolgKosten === 'number', 'price is missing.')
+            assert(typeof categorie === 'string', 'categorie is missing')
             next()
         } catch (err) {
             res.status(400).json({
@@ -22,16 +23,16 @@ let controller = {
     createWorkshop(req, res, next) {
         logger.info('createworkshop called')
         const workshop = req.body
-        let { naam, beschrijving, kosten, vervolgKosten, genre } = workshop
+        let { naam, beschrijving, kosten, vervolgKosten, categorie } = workshop
         console.log('workshop =', workshop)        
 
         let sqlQuery =
-        'INSERT INTO `Workshop` (`Naam`, `Beschrijving`, `Kosten`, `Vervolg Kosten`, `genre`) VALUES (?, ?, ?, ?, ?)'
+        'INSERT INTO `Workshop` (`Naam`, `Beschrijving`, `Kosten`, `Vervolg Kosten`, `Categorie`) VALUES (?, ?, ?, ?, ?)'
         logger.debug('createWorkshop', 'sqlQuery =', sqlQuery)
 
         connection.query(
             sqlQuery,
-            [ naam, beschrijving, kosten, vervolgKosten, genre ],
+            [ naam, beschrijving, kosten, vervolgKosten, categorie ],
             (error, results, fields) => {
                 
                 connection.release()
@@ -85,7 +86,127 @@ let controller = {
                 }
             }
         )
+    },
+
+    getOne(req, res, next) {
+        const workshopName = req.params.workshopName;
+
+        const query =
+            `SELECT Naam, Beschrijving, Kosten, Vervolg Kosten, Categorie FROM Workshop WHERE Naam = '` +
+            workshopName +
+            `';`;
+
+        logger.info('getOne:', workshopName);
+
+        connection.connectDatabase(query, (error, results, fields) => {
+            if (error) {
+                logger.debug(workshopName, query, error);
+                res.status(400).json({
+                    message: 'workshop does not exist!'
+                });
+            } else {
+                res.status(200).json({
+                    User: results[0]
+                });
+            }
+        });
+    },
+
+    getAll(req, res, next) {
+        const query = `SELECT Naam, Beschrijving, Kosten, Vervolg Kosten, Categorie FROM Workshop;'`
+
+        connection.connectDatabase(query, (error, results, fields) => {
+            if (error) {
+                logger.debug('getAll', query);
+                res.status(400).json({
+                    error: error
+                });
+            } else if (results.length == 0) {
+                res.status(200).json({
+                    message: 'There are no workshops!'
+                });
+            } else {
+                res.status(200).json({
+                    Users: results
+                });
+            }
+        });
+    },
+
+    checkDatabase(req, res, next) {
+        const workshopName = req.params.workshopName;
+
+        const query =
+            `SELECT Naam FROM Workshop WHERE Naam = '` + workshopName + `';`;
+
+        connection.connectDatabase(query, (error, results, fields) => {
+            if (results.length == 0) {
+                logger.debug('checkDatabase:', error);
+                res.status(400).json({
+                    message: 'Workshop not found!'
+                });
+            } else {
+                logger.debug('Workshop found, continuing!');
+                next();
+            }
+        });
+    },
+
+    validateUpdateUser(req, res, next) {
+        let { Naam, Email, Organisatie, Adress } = req.body;
+
+        logger.info('validateUpdateUser:', req.body);
+        try {
+            assert(typeof naam === 'string', 'name is missing.')
+            assert(typeof beschrijving === 'string', 'description is missing.')
+            assert(typeof Kosten === 'number', 'tijdsduur is missing.')
+            assert(typeof vervolgKosten === 'number', 'price is missing.')
+            assert(typeof categorie === 'string', 'categorie is missing')
+            next()
+        } catch (err) {
+            logger.debug('Error updating workshop:', err.message);
+            res.status(400).json({
+                message: 'Error updating workshop!',
+                error: err.message
+            });
+        }
+    },
+
+    updateUser(req, res, next) {
+        const workshopName = req.params.workshopName;
+        logger.info('updateWorkshop', workshopName);
+
+        let { naam, beschrijving, kosten, vervolgKosten, categorie } = req.body;
+        let query =
+            `UPDATE Workshop SET Naam = ?, Besschrijving = ?, Kosten = ?, VervolgKosten = ?, Categorie = ? WHERE name = '` +
+            workshopName +
+            `';`;
+
+        connection.connectDatabase(
+            query,
+            [ naam, beschrijving, kosten, vervolgKosten, categorie ],
+            (error, results, fields) => {
+                if (error) {
+                    logger.debug('updateWorkshop:', workshopName, req.body, error);
+                    res.status(400).json({
+                        message: 'A workshop with this email already exists!',
+                        error: error
+                    });
+                } else {
+                    logger.info('Workshop updated:', req.body);
+                    res.status(200).json({
+                        message: 'Workshop updated!',
+                        result: {
+                            ...req.body
+                        },
+                        test: query
+                    });
+                }
+            }
+        );
     }
 }
+
+
 
 module.exports = controller
--- src/routes/workshop.routes.js
+++ src/routes/workshop.routes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const workshopcontroller = require('../controllers/workshop.controller')
 
-router.post('/create', workshopcontroller.validateWorkshop,workshopcontroller.createWorkshop)
+router.post('/create', workshopcontroller.validateWorkshop, workshopcontroller.createWorkshop)
 router.post('/delete', workshopcontroller.deleteWorkshop)
+router.post('/getOne', workshopcontroller.getOne)
+router.post('/getAll', workshopcontroller.getAll)
+router.post('/update', workshopcontroller.validateUpdateUser, workshopcontroller.updateUser)
 
 module.exports = router
