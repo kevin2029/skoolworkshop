44993012671f724692787d16f677cdbfde5d96ce
jwruijzenaars <jw.ruijzenaars@student@avans.nl>
2020-06-04 12:39:52 +0200

    Update workshop.routes.js

--- src/routes/workshop.routes.js
+++ src/routes/workshop.routes.js
@@ -3,15 +3,10 @@ const router = express.Router();
 const workshopcontroller = require('../controllers/workshop.controller');
 // authcontroller
 
-router.post(
-    '/workshop',
-    workshopcontroller.validateWorkshop,
-    workshopcontroller.createWorkshop
-);
-router.delete(
-    '/workshop/:workshopNaam',
-    workshopcontroller.checkDatabase,
-    workshopcontroller.deleteWorkshop
-);
+router.post('/workshop', workshopcontroller.validateWorkshop, workshopcontroller.createWorkshop)
+router.delete('/delete/:naamWorkshop', workshopcontroller.deleteWorkshop)
+router.get('/getOne', workshopcontroller.getOne)
+router.get('/getAll', workshopcontroller.getAll)
+router.put('/update/:naamWorkshop', workshopcontroller.validateUpdateWorkshop, workshopcontroller.updateWorkshop)
 
 module.exports = router;
