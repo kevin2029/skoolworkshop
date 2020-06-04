3eec5b16a05f5b245adc3810d88907f20046e318
jwruijzenaars <jw.ruijzenaars@student@avans.nl>
2020-06-04 12:57:42 +0200

    hotfix workshop

--- src/controllers/workshop.controller.js
+++ src/controllers/workshop.controller.js
@@ -38,7 +38,7 @@ let controller = {
         logger.debug('workshop =', workshop);
 
         let sqlQuery =
-            'INSERT INTO `Workshop` (`Naam`, `Beschrijving`, `Kosten`, `Vervolg Kosten`, `categorie`) VALUES (?, ?, ?, ?, ?)';
+            'INSERT INTO `Workshop` (`Naam`, `Beschrijving`, `Kosten`, `VervolgKosten`, `categorie`) VALUES (?, ?, ?, ?, ?)';
         logger.debug('createWorkshop', 'sqlQuery =', sqlQuery);
 
         connection.connectDatabase(
@@ -65,7 +65,7 @@ let controller = {
     },
 
     checkDatabase(req, res, next) {
-        const workshopName = req.params.workshopNaam;
+        const workshopName = req.params.naamWorkshop;
 
         let sqlQuery =
             `SELECT Naam FROM Workshop WHERE Naam = '` + workshopName + `'`;
@@ -87,7 +87,7 @@ let controller = {
 
     deleteWorkshop(req, res, next) {
         logger.info('deleteWorkshop called');
-        const workshopName = req.params.workshopNaam;
+        const workshopName = req.params.naamWorkshop;
 
         let sqlQuery =
             `DELETE FROM Workshop WHERE Naam = '` + workshopName + `'`;
@@ -106,7 +106,129 @@ let controller = {
                 });
             }
         });
+    },
+
+
+getOne(req, res, next) {
+    const workshopName = req.params.naamWorkshop;
+
+    const query =
+        `SELECT Naam, Beschrijving, Kosten, VervolgKosten, Categorie FROM Workshop WHERE Naam = '` +
+        workshopName +
+        `';`;
+
+    logger.info('getOne:', workshopName);
+
+    connection.connectDatabase(query, (error, results, fields) => {
+        if (error) {
+            logger.debug(workshopName, query, error);
+            res.status(400).json({
+                message: 'workshop does not exist!'
+            });
+        } else {
+            res.status(200).json({
+                Workshop: results[0]
+            });
+        }
+    });
+},
+
+getAll(req, res, next) {
+    const query = `SELECT Naam, Beschrijving, Kosten, VervolgKosten, Categorie FROM Workshop;`
+
+    connection.connectDatabase(query, (error, results, fields) => {
+        if (error) {
+            logger.debug('getAll', query);
+            res.status(400).json({
+                error: error
+            });
+        } else if (results.length == 0) {
+            res.status(200).json({
+                message: 'There are no workshops!'
+            });
+        } else {
+            res.status(200).json({
+                Workshops: results
+            });
+        }
+    });
+},
+
+checkDatabase(req, res, next) {
+    const workshopName = req.params.naamWorkshop;
+
+    const query =
+        `SELECT Naam FROM Workshop WHERE Naam = '` + workshopName + `';`;
+
+    connection.connectDatabase(query, (error, results, fields) => {
+        if (results.length == 0) {
+            logger.debug('checkDatabase:', error);
+            res.status(400).json({
+                message: 'Workshop not found!'
+            });
+        } else {
+            logger.debug('Workshop found, continuing!');
+            next();
+        }
+    });
+},
+
+validateUpdateWorkshop(req, res, next) {
+    let { naamWorkshop, beschrijving, kosten, vervolgKosten, categorie } = req.body;
+
+    logger.info('validateUpdateWorkshop:', req.body);
+    try {
+        assert(typeof naamWorkshop === 'string', 'name is missing.')
+        assert(typeof beschrijving === 'string', 'description is missing.')
+        assert(typeof kosten === 'string', 'kosten is missing.')
+        assert(typeof vervolgKosten === 'string', 'price is missing.')
+        assert(typeof categorie === 'string', 'categorie is missing')
+        next()
+    } catch (err) {
+        logger.debug('Error updating workshop:', err.message);
+        res.status(400).json({
+            message: 'Error updating workshop!',
+            error: err.message
+        });
     }
+},
+
+updateWorkshop(req, res, next) {
+    const workshopName = req.params.naamWorkshop;
+    logger.info('updateWorkshop', workshopName);
+
+    let { naamWorkshop, beschrijving, kosten, vervolgKosten, categorie } = req.body;
+    let query =
+        `UPDATE Workshop SET Naam = ?, Beschrijving = ?, Kosten = ?, VervolgKosten = ?, Categorie = ? WHERE Naam = '` +
+        workshopName +
+        `';`;
+    logger.debug("query: ", query);
+
+    connection.connectDatabase(
+        query,
+        [ naamWorkshop, beschrijving, kosten, vervolgKosten, categorie ],
+        (error, results, fields) => {
+            if (error) {
+                logger.debug('updateWorkshop:', workshopName, req.body, error);
+                res.status(400).json({
+                    message: 'A workshop with this email already exists!',
+                    error: error
+                });
+            } else {
+                logger.info('Workshop updated:', req.body);
+                res.status(200).json({
+                    message: 'Workshop updated!',
+                    result: {
+                        ...req.body
+                    },
+                    test: query
+                });
+            }
+        }
+    );
+}
 };
 
-module.exports = controller;
+
+
+module.exports = controller
--- src/routes/workshop.routes.js
+++ src/routes/workshop.routes.js
@@ -4,9 +4,9 @@ const workshopcontroller = require('../controllers/workshop.controller');
 // authcontroller
 
 router.post('/workshop', workshopcontroller.validateWorkshop, workshopcontroller.createWorkshop)
-router.delete('/delete/:naamWorkshop', workshopcontroller.deleteWorkshop)
-router.get('/getOne', workshopcontroller.getOne)
-router.get('/getAll', workshopcontroller.getAll)
-router.put('/update/:naamWorkshop', workshopcontroller.validateUpdateWorkshop, workshopcontroller.updateWorkshop)
+router.delete('/workshop/:naamWorkshop', workshopcontroller.deleteWorkshop)
+router.get('/workshop/:naamWorkshop', workshopcontroller.getOne)
+router.get('/workshop', workshopcontroller.getAll)
+router.put('/workshop/:naamWorkshop', workshopcontroller.validateUpdateWorkshop, workshopcontroller.updateWorkshop)
 
 module.exports = router;
