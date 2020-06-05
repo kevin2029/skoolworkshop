6d1e33a739464421b23a7a771f1f48de289c21ec
Sam van der Flaas <s.vanderflaas@student.avans.nl>
2020-06-05 11:47:42 +0200

    Update factuur.php

--- src/frontend/facturenaanmaken/factuur.php
+++ src/frontend/facturenaanmaken/factuur.php
@@ -37,7 +37,7 @@ function upload (){
   } else {
    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
       echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-      return true;
+      return $target_file;
     } else {
      echo "Sorry, there was an error uploading your file.";
      return false;
