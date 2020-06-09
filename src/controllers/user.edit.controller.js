41d1293d269591ab73eb31e7ce478d70b20296fd
Somfic <somfic@gmail.com>
2020-06-09 14:54:38 +0200

    Syntax aangepast

--- src/controllers/user.edit.controller.js
+++ src/controllers/user.edit.controller.js
@@ -1,27 +1,28 @@
 const assert = require('assert');
 const connection = require('../config/database.connection');
 const logger = require('../config/config').logger;
+const bcrypt = require('bcrypt');
 
 let controller = {
-    
     validateEdit(req, res, next) {
-        let { Naam, Email, Organisatie, Adress, Wachtwoord } = req.body;
+        let { Naam, Organisatie, Adress, Wachtwoord } = req.body;
+        let Email = req.email;
 
         let edits = 0;
 
-        if(typeof Naam === 'string') {
+        if (typeof Naam === 'string') {
             edits++;
         }
 
-        if(typeof Organisatie === 'string') {
+        if (typeof Organisatie === 'string') {
             edits++;
         }
 
-        if(typeof Adress === 'string') {
+        if (typeof Adress === 'string') {
             edits++;
         }
 
-        if(typeof Wachtwoord === 'string') {
+        if (typeof Wachtwoord === 'string') {
             edits++;
         }
 
@@ -42,63 +43,64 @@ let controller = {
     },
 
     editUser(req, res, next) {
-        let { Naam, Organisatie, Adress, Wachtwoord, Email } = req.body;
+        let { Naam, Organisatie, Adress, Wachtwoord } = req.body;
+        let Email = req.email;
 
-        let query = "UPDATE `Gebruiker` SET";
+        let query = 'UPDATE `Gebruiker` SET';
         let values = [];
 
-        if(typeof Naam === 'string') {
-           query += ' `Naam` = ?'
-           values.push(Naam);
+        if (typeof Naam === 'string') {
+            query += ' `Naam` = ?';
+            values.push(Naam);
         }
 
-        if(typeof Organisatie === 'string') {
-            query += ' `Organisatie` = ?'
+        if (typeof Organisatie === 'string') {
+            query += ' `Organisatie` = ?';
             values.push(Organisatie);
         }
 
-        if(typeof Adress === 'string') {
-            query += ' `Adress` = ?'
+        if (typeof Adress === 'string') {
+            query += ' `Adress` = ?';
             values.push(Adress);
         }
 
-        if(typeof Wachtwoord === 'string') {
-            query += ' `Wachtwoord` = ?'
+        if (typeof Wachtwoord === 'string') {
+            query += ' `Wachtwoord` = ?';
             values.push(Wachtwoord);
+
+            bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash(req.body.Wachtwoord, salt, function (err, hash) {
+                 // vanaf hier pas connectie doen tenzij er geen wachtwoord meegegeven is???
+                });
+            });
         }
 
         query += ' WHERE `Email` = ?';
         values.push(Email);
 
-        connection.connectDatabase(
-            query,
-            values,
-            (error, results, fields) => {
-                if (error) {
-                    logger.debug('editUser:', req.body, error);
-                    res.status(400).json({
-                        message: 'Could not edit user!',
-                        error: error
-                    });                    
-                } 
-                else if (results.affectedRows == 0) {
-                    logger.debug('editUser: email not in use');
-                    res.status(400).json({
-                        message: 'No user with that email exists!'
-                    });    
-                }
-                else {
-                    logger.info('User edited:', req.body);
-                    res.status(200).json({
-                        message: `User edited with ${results.changedRows} changes`,
-                        result: {
-                          ...req.body
-                        }
-                    });
-                }
+        connection.connectDatabase(query, values, (error, results, fields) => {
+            if (error) {
+                logger.debug('editUser:', req.body, error);
+                res.status(400).json({
+                    message: 'Could not edit user!',
+                    error: error
+                });
+            } else if (results.affectedRows == 0) {
+                logger.debug('editUser: email not in use');
+                res.status(400).json({
+                    message: 'No user with that email exists!'
+                });
+            } else {
+                logger.info('User edited:', req.body);
+                res.status(200).json({
+                    message: `User edited with ${results.changedRows} changes`,
+                    result: {
+                        ...req.body
+                    }
+                });
             }
-        );
+        });
     }
 };
 
-module.exports = controller
+module.exports = controller;
