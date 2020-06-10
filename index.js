4ee5785522ee708b6eeb48f8828013fdfac66de1
Sam van der Flaas <s.vanderflaas@student.avans.nl>
2020-06-10 12:52:01 +0200

    .

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/frontend/gebruikersaanmaken/test.html');
+    response.sendFile(__dirname + '/src/frontend/workshopdelete/index.html');
 });
 
 // Serve files from the ./static folder
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,10 +4,14 @@ const usercontroller = require('../controllers/user.controller');
 const authenticationcontroller = require('../controllers/authenication.controller');
 
 router.post('/create', usercontroller.validateUser, usercontroller.createUser);
-router.post('/delete', usercontroller.checkDatabase, usercontroller.deleteUser);
-router.post('/getone', usercontroller.getOne);
-router.post('/getall', usercontroller.getAll);
-router.post(
+router.delete(
+    '/delete',
+    usercontroller.checkDatabase,
+    usercontroller.deleteUser
+);
+router.get('/getone', usercontroller.getOne);
+router.get('/getall', usercontroller.getAll);
+router.put(
     '/update',
     usercontroller.checkDatabase,
     usercontroller.validateUpdateUser,
