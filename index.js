f2d9253dda92c56225115539f0f54a6f1f84299d
Sam van der Flaas <s.vanderflaas@student.avans.nl>
2020-06-10 13:09:14 +0200

    .

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/src/frontend/workshopdelete/index.html');
+    response.sendFile(__dirname + '/frontend/index.html');
 });
 
 // Serve files from the ./static folder
