abc700f5e673f2e17d5ebbbc40467d9c4f77f049
kevin2029 <46976260+kevin2029@users.noreply.github.com>
2020-06-15 14:42:55 +0200

    beheerdfer aanmaken dashboeasrd

--- frontend/scripts/dropdownGetter.js
+++ frontend/scripts/dropdownGetter.js
@@ -29,3 +29,35 @@ function getOrganisation() {
             );
         });
 }
+
+function getUserName() {
+    sendGetRequest(apiUrl + '/api/user/getall')
+        .then((data) => {
+            console.log(data.result);
+            let tableBody = document.getElementById('Organisatie');
+
+            let rows =
+                '<option value="" disabled selected hidden>Kies een persoon</option>';
+            if (data.Users) {
+                data.Users.forEach((item) => {
+                    console.log(item);
+                    rows +=
+                        // Column namen van de data die je opvraagt
+                        `<option value="${item.Naam}">${item.Naam}</option>`;
+                    console.log(item.Naam);
+                });
+
+                tableBody.innerHTML = rows; // Stop de aangemaakte tablerows in de body van de eerder gemaakt table
+            } else {
+                let niks = '';
+                tableBody.innerHTML = niks;
+            }
+        })
+        .catch((error) => {
+            alert('Error fetching name!');
+            console.log(
+                'There has been a problem with your fetch operation:',
+                error
+            );
+        });
+}
