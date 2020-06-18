202fe7afc79dab2dc386edf1d6f903db2559f7b0
Somfic <somfic@gmail.com>
2020-06-18 09:57:51 +0200

    Fix api url

--- frontend/dashboardBeheerder/factuur/facturenaanmaken/factuur.php
+++ frontend/dashboardBeheerder/factuur/facturenaanmaken/factuur.php
@@ -4,7 +4,7 @@ $target_dir = "..\\..\\..\\upload\\facturen\\";
 // IsBetaald == true  -> 1
 // IsBetaald == false -> 0
 $isBetaald = 0;
-if($_POST['IsBetaald'] == 'true') {
+if ($_POST['IsBetaald'] == 'true') {
   $isBetaald = 1;
 }
 
@@ -16,9 +16,9 @@ if (($_FILES['fileToUpload']['name'] != "")) {
   $path = pathinfo($file);
   $filename = $path['filename'];
   $ext = $path['extension'];
-  
+
   // Check ofdat bestand PDF is.
-  if(strtolower($ext) != 'pdf') {
+  if (strtolower($ext) != 'pdf') {
     echo "Sorry, file is not PDF.";
     exit();
   }
@@ -33,13 +33,12 @@ if (($_FILES['fileToUpload']['name'] != "")) {
   if (file_exists($perm_file)) {
     echo "Sorry, file already exists.";
     exit();
-
   } else {
     move_uploaded_file($temp_file, $perm_file);
     echo "File uploaded successfully.";
 
     // Registreer bestand in API.
-    post('localhost:3000/api/invoice', ['GebruikerMail' => $_POST['GebruikerMail'], 'Path' => $filename . "." . $ext, 'IsBetaald' => $isBetaald]);
+    post('http://skoolworkshop-c2.herokuapp.com//api/invoice', ['GebruikerMail' => $_POST['GebruikerMail'], 'Path' => $filename . "." . $ext, 'IsBetaald' => $isBetaald]);
 
     exit();
   }
@@ -48,11 +47,11 @@ if (($_FILES['fileToUpload']['name'] != "")) {
 // Executeerd een post request.
 function post($url, $data)
 {
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($curl);
+  curl_close($curl);
+  return $response;
 }
